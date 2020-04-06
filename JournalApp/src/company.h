00842468d07461e4bf759f495726e1a1ed2f4985
Michal Cam <mcamromer@gmail.com>
2020-04-06 18:16:23 +0200

    src/company.h: Insert declaration of Company class.

---
+++ JournalApp/src/company.h
@@ -0,0 +1,27 @@
+#ifndef COMPANY_H
+#define COMPANY_H
+#include "position.h"
+#include <cstdio>
+#include <cstdint>
+
+class Company
+{
+private:
+    char company_name[100];
+    Position* Positions;
+    uint32_t num_of_positions;
+
+public:
+    Company();
+    ~Company();
+    char* get_name();
+    void set_name(const char *name);
+    int get_num_of_positions(FILE* cv_list, int point_pos, bool getFromCvList);
+    int add_new_positions(int num_of_pos);
+    void set_positions_list(FILE* cv_list, int point_pos);
+    void print_positions();
+    void save_new_positions_in_cv_list(FILE* cv_list);
+    void clean_company_data();
+};
+
+#endif
