f1b55c6adbb329a1b299fa8649803d99acebb789
Michal Cam <mcamromer@gmail.com>
2020-04-07 11:19:30 +0200

    src/jaobjects.h: Add new argument isRecordNew for get_num_of_companies function.

--- JournalApp/src/jaobjects.h
+++ JournalApp/src/jaobjects.h
@@ -41,7 +41,8 @@ public:
     int get_day();
     int get_month();
     int get_year();
-    int get_num_of_companies(FILE* cv_list, int point_pos, bool getFromCvList);
+    int get_num_of_companies(FILE* cv_list, int point_pos,
+    		bool getFromCvList, bool isRecordNew);
     int set_list_of_companies(FILE* cv_list, int point_pos);
     int add_new_companies(int num_of_comps);
 
