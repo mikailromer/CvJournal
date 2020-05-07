d91f3afe752949c1f26a44cd10c63f7fd6dacc80
Michal Cam <mcamromer@gmail.com>
2020-05-07 21:00:56 +0200

    JournalApp/src/position.h: Remove declarations of class methods. Convert Position class declaration to struct declaration. Add the declaration of init_positions function.

--- JournalApp/src/position.h
+++ JournalApp/src/position.h
@@ -1,15 +1,21 @@
 #ifndef POSITION_H
 #define POSITION_H
 
-class Position
+typedef struct
 {
-private:
-    char position_name[100];
-public:
-    char* get_position();
-    void set_position_name(const char* name);
-    void print_position_name(int pos_name);
-    int clean_position();
-};
+	char position_name[100];
+	char* (*get_position_ptr)();
+	void (*set_position_name_ptr)(const char* name);
+	void (*print_position_name_ptr)(int pos_name);
+	int (*clean_position_ptr)();
+}Position;
 
+int init_positions(Position** Positions,int num_of_pos,
+		int num_of_new_pos, bool isPositionTableExist);
+/*
+static char* get_position();
+static void set_position_name(const char* name);
+static void print_position_name(int pos_name);
+static int clean_position();
+*/
 #endif
