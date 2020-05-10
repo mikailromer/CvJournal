c5fa5c50426efc664d88ed5186a15576ce7c35f2
Michal Cam <mcamromer@gmail.com>
2020-05-10 14:25:55 +0200

    src/company.cpp: Implement in the Company class methods, pointers to function, which set the name of position for the given Company class.

--- JournalApp/src/company.cpp
+++ JournalApp/src/company.cpp
@@ -3,11 +3,6 @@
 #include <cstring>
 #include <cstdint>
 
-Company::Company()
-{
-    Positions = NULL;
-}
-
 int Company::get_num_of_positions(FILE* cv_list, int point_pos,
         bool getFromCvList, bool isCompanyNew)
 {
@@ -32,22 +27,13 @@ int Company::get_num_of_positions(FILE* cv_list, int point_pos,
 int Company::add_new_positions(int num_of_new_pos)
 {
     char row[100];
-
-    if(!num_of_positions) Positions = NULL;
-    Positions = (Position*) realloc(Positions, sizeof(Position) *
-            (num_of_positions + num_of_new_pos));
-    if(!Positions)
-    {
-        printf("Memory reallocation for Positions class variable has failed.\n");
-        return -1;
-    }
-
+    init_positions(&Positions,num_of_positions, num_of_new_pos);
     for(uint i = num_of_positions; i < num_of_positions + num_of_new_pos; i++)
     {
         printf("\nYou need to give %i positions.\n", num_of_new_pos);
         printf("Type the name of the position No.%i: ", i+1);
         fgets(row, 100, stdin);
-        Positions[i].set_position_name(row);
+        Positions[i].set_position_name_ptr(Positions[i].position_name, row);
         memset(row, '\0', sizeof(row));
     }
 
@@ -60,7 +46,7 @@ void Company::set_positions_list(FILE* cv_list, int point_pos)
     char row[100];
     char* p;
     int position_num = -1;
-    Positions = (Position*) malloc(sizeof(Position) * num_of_positions);
+    init_positions(&Positions, 0, num_of_positions );
     for (;;)
     {
         fgets(row, sizeof(row), cv_list);
@@ -69,7 +55,7 @@ void Company::set_positions_list(FILE* cv_list, int point_pos)
         {
             p += 10;
             position_num++;
-            Positions[position_num].set_position_name(p);
+            Positions[position_num].set_position_name_ptr(Positions[position_num].position_name, p);
         }
         if (strstr(row, "Date: ") || strstr(row, "Company: ") || feof(cv_list)) break;
         memset(row,'\0',sizeof(row));
@@ -93,7 +79,7 @@ void Company::print_positions()
 {
     for(uint i = 0; i < num_of_positions; i++)
     {
-        printf("\t\tPosition No.%i: %s\n", i + 1, Positions[i].get_position());
+        printf("\t\tPosition No.%i: %s\n", i + 1, Positions[i].position_name);
     }
 }
 
@@ -101,7 +87,7 @@ void Company::save_new_positions_in_cv_list(FILE* cv_list)
 {
     for(uint i = 0; i < num_of_positions; i++)
     {
-        fprintf(cv_list, "\n\t\tPosition: %s",Positions[i].get_position());
+        fprintf(cv_list, "\n\t\tPosition: %s",Positions[i].position_name);
     }
     fprintf(cv_list, "\n");
 }
@@ -110,7 +96,7 @@ void Company::clean_company_data()
 {
     for(uint i = 0; i < num_of_positions; i++)
     {
-        Positions[i].clean_position();
+    	memset(Positions[i].position_name, '\0', sizeof(Positions[i].position_name));
     }
     free(Positions);
 
