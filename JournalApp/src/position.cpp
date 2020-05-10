93833e67f62202b1ae9492b3c0bc395d979fc5da
Michal Cam <mcamromer@gmail.com>
2020-05-10 14:21:39 +0200

    src/position.cpp: Define set_position_name static function.

--- JournalApp/src/position.cpp
+++ JournalApp/src/position.cpp
@@ -3,21 +3,23 @@
 #include <cstdlib>
 #include <cstring>
 
+static void set_position_name(char* position_name, const char* name);
+
 int init_positions(Position** Positions,int num_of_pos,
-		int num_of_new_pos, bool isPositionTableExist)
+		int num_of_new_pos)
 {
 	int i, begin, end;
-	if(!isPositionTableExist) *Positions = NULL;
-	*Positions = (Position*)realloc(Positions,sizeof(Position) *
+	if(num_of_pos == 0) *Positions = NULL;
+	*Positions = (Position*)realloc(*Positions,sizeof(Position) *
 			(num_of_pos + num_of_new_pos));
 
-	if(*Positions)
+	if(*Positions == NULL)
 	{
 		printf("Memory reallocation for Positions struct variable has failed.\n");
 		return -1;
 	}
 
-	if(!isPositionTableExist)
+	if(num_of_pos == 0)
 	{
 		begin = 0;
 		end = num_of_new_pos;
@@ -30,20 +32,14 @@ int init_positions(Position** Positions,int num_of_pos,
 
 	for(i = begin; i < end; i++)
 	{
-		memset((*Positions)->position_name,'\0', sizeof((*Positions)->position_name));
+		(*Positions)[i].set_position_name_ptr = set_position_name;
+		memset((*Positions)[i].position_name,'\0', sizeof((*Positions)[i].position_name));
 	}
 
 	return 0;
 }
 
-/*
-
-static char* get_position()
-{
-    return position_name;
-}
-
-static void set_position_name(const char* name)
+static void set_position_name(char* position_name, const char* name)
 {
     strcpy(position_name, name);
     if( position_name[strlen(position_name) - 1] == '\n')
@@ -51,10 +47,3 @@ static void set_position_name(const char* name)
         position_name[strlen(position_name) - 1] = '\0';
     }
 }
-
-static int clean_position()
-{
-    memset(position_name, '\0', sizeof(position_name));
-    return 0;
-}
-*/
