e1df017a9fb21e8aa686a43acaa38c6751b71a13
Michal Cam <mcamromer@gmail.com>
2020-05-11 14:15:40 +0200

    src/company.h: Declare pointers to static functions, used for Company struct members modification, save Company structs data into the CV list file, printing Company structs data on the the Console.

--- JournalApp/src/company.h
+++ JournalApp/src/company.h
@@ -2,8 +2,26 @@
 #define COMPANY_H
 #include "position.h"
 #include <cstdio>
+#include <cstdlib>
 #include <cstdint>
 
+typedef struct
+{
+    char company_name[100];
+    Position* Positions;
+    uint32_t num_of_positions;
+    int (*get_num_of_positions_ptr)(FILE* ,int* ,int ,bool ,bool );
+    int (*add_new_positions_ptr)(Position** ,uint32_t* ,int );
+    void (*set_positions_list_ptr)(FILE* ,Position** ,int ,int );
+    void (*set_name_ptr)(char*, const char* );
+    void (*print_positions_ptr)(Position* , int );
+    void (*save_new_positions_in_cv_list_ptr)(FILE* , Position* , int );
+    void (*clean_company_data_ptr)(Position** ,uint* ,char* );
+}Company;
+
+int init_companies(Company** Companies,int num_of_comps,
+		int num_of_new_comps);
+/*
 class Company
 {
 private:
@@ -24,5 +42,5 @@ public:
     void save_new_positions_in_cv_list(FILE* cv_list);
     void clean_company_data();
 };
-
+*/
 #endif
