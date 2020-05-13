02c50f57b403c9ccc2513334cf1168e4a94dd046
Michal Cam <mcamromer@gmail.com>
2020-05-13 12:46:37 +0200

    src/JournalApp.cpp: From the CV Journal Application main code, use pointers to the static functions, which handle Record structs data and load/save the from/to the CV list.

--- JournalApp/src/JournalApp.cpp
+++ JournalApp/src/JournalApp.cpp
@@ -5,7 +5,6 @@
 #include <cctype>
 
 #include "record.h"
-//#include "jaobjects.h"
 
 void print_record_data(Record* Records, int rec_num);
 void print_all_recorded_data(Record* Records, int num_of_records);
@@ -16,7 +15,6 @@ int num_of_records(const char* filename);
 int initialize_cv_list_file(const char* filename);
 int clean_record_table(Record* Records, int sum_of_records);
 
-
 int main()
 {
 
@@ -26,7 +24,7 @@ int main()
     FILE* cv_list = fopen(filename,"r");
     char choice;
     char chosen_rec_num[4];
-    int rc, num;
+    uint rc, num;
     if(!cv_list)
     {
         printf("The cv_list.txt file doesnt't exist.\n");
@@ -42,7 +40,7 @@ int main()
     else fclose(cv_list);
 
     sum_of_records = num_of_records(filename);
-    Records = (Record*) realloc(Records, sizeof(Record) * sum_of_records);
+    init_records(&Records, 0, sum_of_records);
     parse_cv_list(filename,Records, sum_of_records);
 
     do{
@@ -66,7 +64,7 @@ int main()
                 fgets(chosen_rec_num, sizeof(chosen_rec_num), stdin);
                 num = atoi(chosen_rec_num);
                 memset(chosen_rec_num, '\0', sizeof(chosen_rec_num));
-            }while(num < 1 || num > sum_of_records);
+            } while(num < 1 || num > sum_of_records);
             
             print_record_data(Records, num);
             break;
@@ -78,7 +76,9 @@ int main()
         case '3':
             for (uint i = 0; i < sum_of_records; i++)
             {
-                Records[i].clean_record_data();
+                Records[i].clean_record_data_ptr(&(Records[i].Companies),
+                        &(Records[i].num_of_companies), &(Records[i].day),
+                        &(Records[i].month), &(Records[i].year));
             }
 
             free(Records);
@@ -86,7 +86,7 @@ int main()
 
             add_new_record(filename);
             sum_of_records++;
-            Records = (Record*) realloc(Records, sizeof(Record) * sum_of_records);
+            init_records(&Records, 0, sum_of_records);
             parse_cv_list(filename, Records, sum_of_records);
             break;
 
@@ -96,14 +96,14 @@ int main()
                 fgets(chosen_rec_num, sizeof(chosen_rec_num), stdin);
                 num = atoi(chosen_rec_num);
                 memset(chosen_rec_num, '\0', sizeof(chosen_rec_num));
-            }while(num < 1 || num > sum_of_records);
+            } while(num < 1 || num > sum_of_records);
             
             update_record(filename, &Records, num, sum_of_records);
             break;
 
         case '5':
-            printf("\nCV Journal Application is turning off.\n");
             clean_record_table(Records, sum_of_records);
+            printf("\nCV Journal Application is turning off.\n");
             break;
 
         default:
@@ -116,16 +116,20 @@ int main()
 
 void print_record_data(Record* Records, int rec_num)
 {
-    Records[rec_num-1].print_date(rec_num);
-    Records[rec_num-1].print_companies();
+    Records[rec_num-1].print_date_ptr(Records[rec_num-1].day, Records[rec_num-1].month,
+            Records[rec_num-1].year, rec_num);
+    Records[rec_num-1].print_companies_ptr(Records[rec_num-1].Companies,
+            Records[rec_num-1].num_of_companies);
 }
 
 void print_all_recorded_data(Record* Records, int num_of_records)
 {
     for (int i = 0; i < num_of_records; i++)
     {
-        Records[i].print_date(i+1);
-        Records[i].print_companies();
+        Records[i].print_date_ptr(Records[i].day, Records[i].month,
+                    Records[i].year, i+1);
+        Records[i].print_companies_ptr(Records[i].Companies,
+                    Records[i].num_of_companies);
     }
 }
 
@@ -136,7 +140,6 @@ int add_new_record(const char* filename)
     char choice;
     int num_of_comps = 0;
     int date[3];
-
 
     //system("clear");
     printf("\nGive date in the following format - DD/MM/YYYY: ");
@@ -175,20 +178,37 @@ int add_new_record(const char* filename)
         else if(choice == 'N' && num_of_comps == 0)return 1;
     } while(choice != 'N');
 
-    Record new_record;
-    new_record.set_date(date);
-    new_record.get_num_of_companies(NULL, 0, false, true);
-    new_record.add_new_companies(num_of_comps);
+    Record* new_record;
+    init_records(&new_record,0,1);
+    new_record->set_date_ptr(&(new_record->day),&(new_record->month),&(new_record->year), date);
+    new_record->get_num_of_companies_ptr(NULL, &(new_record->num_of_companies), 0, false, true);
+    new_record->add_new_companies_ptr(&(new_record->Companies), &(new_record->num_of_companies),
+            num_of_comps);
 
     FILE* cv_list;
     cv_list = fopen(filename, "a");
     fprintf(cv_list, "\n");
-    fprintf(cv_list, "Date: %02i.%02i.%i", new_record.get_day()
-        , new_record.get_month(),
-        new_record.get_year());
+    fprintf(cv_list, "Date: %02i.%02i.%i", new_record->day
+        , new_record->month,
+        new_record->year);
 
-    new_record.save_new_companies_in_cv_list(cv_list);
-    new_record.clean_record_data();
+    new_record->save_new_companies_in_cv_list_ptr(cv_list, &(new_record->Companies),
+            new_record->num_of_companies);
+    new_record->clean_record_data_ptr(&(new_record->Companies),&(new_record->num_of_companies),
+            &(new_record->day), &(new_record->month), &(new_record->year));
+
+    new_record->get_num_of_companies_ptr= NULL;
+    new_record->set_list_of_companies_ptr = NULL;
+    new_record->add_new_companies_ptr = NULL;
+    new_record->update_company_ptr = NULL;
+    new_record->print_date_ptr = NULL;
+    new_record->set_date_ptr = NULL;
+    new_record->print_companies_ptr = NULL;
+    new_record->save_new_companies_in_cv_list_ptr = NULL;
+    new_record->clean_record_data_ptr = NULL;
+
+    free(new_record);
+    new_record = NULL;
 
     fclose(cv_list);
     return 0;
@@ -237,8 +257,8 @@ int update_record(const char* filename, Record** Records, int rec_index, int sum
         switch (choice_update_option)
         {
         case '1':
-            sum_of_comps = (*Records)[rec_index-1].get_num_of_companies(NULL,
-                    0,false, false);
+            sum_of_comps = (*Records)[rec_index-1].get_num_of_companies_ptr(NULL,
+                    &((*Records)[rec_index-1].num_of_companies), 0,false, false);
             num_of_comps = 0;
             do
             {
@@ -251,11 +271,14 @@ int update_record(const char* filename, Record** Records, int rec_index, int sum
                 else if(choice_yes_or_no == 'N' && sum_of_comps + num_of_comps == 0)return 1;
             } while(choice_yes_or_no != 'N');
 
-            (*Records)[rec_index-1].add_new_companies(num_of_comps);
+            (*Records)[rec_index-1].add_new_companies_ptr(&((*Records)[rec_index-1].Companies),
+                    &((*Records)[rec_index-1].num_of_companies), num_of_comps);
             break;
 
         case '2':
-            sum_of_comps = (*Records)[rec_index-1].get_num_of_companies(NULL,0,false,false);
+            sum_of_comps = (*Records)[rec_index-1].get_num_of_companies_ptr(NULL,
+                    &((*Records)[rec_index-1].num_of_companies), 0, false, false);
+
             do{
                 printf("\nGive the chosen company's index from 1 - %i:",
                         sum_of_comps);
@@ -263,13 +286,12 @@ int update_record(const char* filename, Record** Records, int rec_index, int sum
                 num = atoi(chosen_comp_num);
             }while(num < 1 || num > sum_of_comps);
 
-            (*Records)[rec_index-1].update_company(num);
+            (*Records)[rec_index-1].update_company_ptr(&((*Records)[rec_index-1].Companies), num);
             break;
 
         default:
             break;
         }
-
     }while(choice_update_option != '1' && choice_update_option != '2');
 
     cv_list = fopen(filename, "r+");
@@ -279,11 +301,12 @@ int update_record(const char* filename, Record** Records, int rec_index, int sum
         if(i != rec_index-1)
         {
             fprintf(cv_list, "\n");
-            fprintf(cv_list, "Date: %02i.%02i.%i", (*Records)[i].get_day()
-                , (*Records)[i].get_month(),
-                (*Records)[i].get_year());
+            fprintf(cv_list, "Date: %02i.%02i.%i", (*Records)[i].day
+                , (*Records)[i].month,
+                (*Records)[i].year);
         }
-        (*Records)[i].save_new_companies_in_cv_list(cv_list);
+        (*Records)[i].save_new_companies_in_cv_list_ptr(cv_list, &((*Records)[i].Companies),
+                (*Records)[i].num_of_companies);
     }
 
     fclose(cv_list);
@@ -322,9 +345,14 @@ int parse_cv_list(const char* filename, Record *cv_records, int number_of_record
             }
 
             rec_num++;
-            cv_records[rec_num].set_date(date);
-            N = cv_records[rec_num].get_num_of_companies(cv_list, ftell(cv_list) , true, false);
-            cv_records[rec_num].set_list_of_companies(cv_list, ftell(cv_list));
+            cv_records[rec_num].set_date_ptr(&(cv_records[rec_num].day),
+                    &(cv_records[rec_num].month), &(cv_records[rec_num].year), date);
+
+            N = cv_records[rec_num].get_num_of_companies_ptr(cv_list,
+                    &(cv_records[rec_num].num_of_companies), ftell(cv_list) , true, false);
+
+            cv_records[rec_num].set_list_of_companies_ptr(cv_list, &(cv_records[rec_num].Companies),
+                    &(cv_records[rec_num].num_of_companies), ftell(cv_list));
         }
 
         memset(row,'\0',sizeof(row));
@@ -402,10 +430,10 @@ int clean_record_table(Record* Records, int sum_of_records )
 {
     for (int i = 0; i < sum_of_records; i++)
     {
-        Records[i].clean_record_data();
+        Records[i].clean_record_data_ptr(&(Records[i].Companies), &(Records[i].num_of_companies),
+                &(Records[i].day), &(Records[i].month), &(Records[i].year));
     }
 
     free(Records);
-
     return 0;
 }
